6aaf7cde43a40983c171c9e3001c15fa92601cba
Simon Gapp <simongapp@gmx.de>
2026-04-09 01:14:29 +0200

    adds Python scripts. Refs: #5

---
+++ helpers/core/symbol_name_parameters.py
@@ -0,0 +1,53 @@
+import copy
+
+def make_validation_rules(reference, remove=None):
+    rules = copy.deepcopy(VALIDATION_RULES_GENERAL)
+    rules["Reference"]["value"] = reference
+    if remove:
+        for parameter in remove:
+            del rules[parameter]
+
+    return rules
+
+PASS = 0
+FAIL = 1
+
+VALIDATION_RULES_GENERAL = {
+    "Reference": {"type": "equals", "value": "R"},
+    "Footprint": {"type": "contains_package"},
+    "Datasheet": {"type": "not_empty"},
+    "Mouser": {"type": "not_empty"},
+    "Digikey": {"type": "not_empty"},
+    "LCSC": {"type": "not_empty"},
+}
+
+symbol_name_parameters = {
+    "symbol_type":
+    {
+        "R":
+        {
+            "Value": "str",
+            "Power_in_W": "float",
+            "Tolerance_in_pct": "float",
+            "Package_in_inch": "str",
+            "VALIDATION_RULES" : make_validation_rules("R")
+        },
+        "C":
+        {
+            "Value": "str",
+            "Voltage_in_V": "float",
+            "Tolerance_in_pct": "float",
+            "Dielectric": "str",
+            "Package_in_inch": "str",
+            "VALIDATION_RULES" : make_validation_rules("C")
+        },
+        "L":
+        {
+            "Value": "str",
+            "I_max_in_A": "float",
+            "Tolerance_in_pct": "float",
+            "Package_LxW_in_mm": "str",
+            "VALIDATION_RULES" : make_validation_rules("L", remove=["Footprint"])
+        }
+    }
+}
