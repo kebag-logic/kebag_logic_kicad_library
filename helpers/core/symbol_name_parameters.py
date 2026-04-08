0dbeff70f59bdb736de62c7e11e53e297da73f13
Simon Gapp <simongapp@gmx.de>
2026-04-08 23:24:12 +0200

    updates symbol name parameters for C. Refs: #3

--- helpers/core/symbol_name_parameters.py
+++ helpers/core/symbol_name_parameters.py
@@ -1,5 +1,21 @@
+import copy
+
+def make_validation_rules(reference):
+    rules = copy.deepcopy(VALIDATION_RULES_GENERAL)
+    rules["Reference"]["value"] = reference
+    return rules
+
 PASS = 0
 FAIL = 1
+
+VALIDATION_RULES_GENERAL = {
+    "Reference": {"type": "equals", "value": "R"},
+    "Footprint": {"type": "contains_package"},
+    "Datasheet": {"type": "not_empty"},
+    "Mouser": {"type": "not_empty"},
+    "Digikey": {"type": "not_empty"},
+    "LCSC": {"type": "not_empty"},
+}
 
 symbol_name_parameters = {
     "symbol_type":
@@ -10,19 +26,16 @@ symbol_name_parameters = {
             "Power_in_W": "float",
             "Tolerance_in_pct": "float",
             "Package_in_inch": "str",
-            "VALIDATION_RULES" : {
-                "Reference": {"type": "equals", "value": "R"},
-                "Footprint": {"type": "contains_package"},
-                "Datasheet": {"type": "not_empty"},
-                "Mouser": {"type": "not_empty"},
-                "Digikey": {"type": "not_empty"},
-                "LCSC": {"type": "not_empty"},
-            },
+            "VALIDATION_RULES" : make_validation_rules("R")
         },
         "C":
         {
             "Value": "str",
-            "Tolerance_in_pct": "float"
+            "Voltage_in_V": "float",
+            "Tolerance_in_pct": "float",
+            "Dielectric": "str",
+            "Package_in_inch": "str",
+            "VALIDATION_RULES" : make_validation_rules("C")
         }
     }
 }
